c65160af864e67b5135c32e9a5f0f18835767538
Adib234 <adibadib42@hotmail.com>
2021-01-21 12:17:53 -0500

    just to be safe

--- main.py
+++ main.py
@@ -1,18 +1,46 @@
 from imageai.Classification import ImageClassification
 import os
+from flask import Flask, request
+from flask_cors import CORS
+import logging
+app = Flask(__name__)
+cors = CORS(app)
 
-execution_path = os.getcwd()
+logging.basicConfig(level=logging.DEBUG)
 
-prediction = ImageClassification()
-# prediction.setModelTypeAsResNet50()
-# prediction.setModelTypeAsInceptionV3()
-prediction.setModelTypeAsDenseNet121()
 
-prediction.setModelPath(os.path.join(
-    execution_path, "DenseNet-BC-121-32.h5"))
-prediction.loadModel()
+@app.route('/')
+def hello_world():
+    return {"What's": "up"}
 
-predictions, probabilities = prediction.classifyImage(
-    os.path.join(execution_path, "IMG_4466.JPG"), result_count=5)
-for eachPrediction, eachProbability in zip(predictions, probabilities):
-    print(eachPrediction, " : ", eachProbability)
+
+@app.route('/tags', methods=['GET', 'POST'])
+def put_tags():
+    if request.method == 'POST':
+        app.logger.info('%s is the request', request.json)
+
+        data = request.json
+        app.logger.info('%s is the data', data)
+
+        image = data["image"]
+        app.logger.info('%s is the image', image)
+
+        execution_path = os.getcwd()
+
+        prediction = ImageClassification()
+        # prediction.setModelTypeAsResNet50()
+        # prediction.setModelTypeAsInceptionV3()
+        prediction.setModelTypeAsDenseNet121()
+
+        prediction.setModelPath(os.path.join(
+            execution_path, "DenseNet-BC-121-32.h5"))
+        prediction.loadModel()
+
+        predictions, probabilities = prediction.classifyImage(
+            os.path.join(execution_path, image), result_count=5)
+
+        final = []
+        for eachPrediction, eachProbability in zip(predictions, probabilities):
+            final.append(eachPrediction)
+
+        return {'tags': final}

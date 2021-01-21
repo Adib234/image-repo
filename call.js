70c6d6d532b1e8f02218d8d00b344d24b758f44a
Adib234 <adibadib42@hotmail.com>
2021-01-21 12:16:43 -0500

    backend fails when it get base 64 encoded image

--- call.js
+++ call.js
@@ -2,7 +2,9 @@ const axios = require("axios");
 
 async function getImagePrediction() {
   try {
-    const response = await axios.get("http://127.0.0.1:5000/tags");
+    const response = await axios.post("http://127.0.0.1:5000/tags", {
+      image: "baboon.png",
+    });
     console.log(response.data.tags);
   } catch (e) {
     console.log(e);

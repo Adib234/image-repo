a4a5dda4b283ae4de18637eeb037c3deeedc4dc9
Adib234 <adibadib42@hotmail.com>
2021-01-23 19:48:50 -0500

    cleaned up repo

--- model.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-const ParkSchema = new Schema({
-  name: String,
-  location: String,
-  ranking: Number,
-});
-module.exports = mongoose.model("park", ParkSchema);
